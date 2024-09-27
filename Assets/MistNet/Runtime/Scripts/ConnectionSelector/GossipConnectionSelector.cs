d80794750d9667bbe5d30b067b92460b9641d24d
fog <38463346+fog-zs@users.noreply.github.com>
2024-09-27 15:03:07 +0900

    add sending messages

--- Assets/MistNet/Runtime/Scripts/ConnectionSelector/GossipConnectionSelector.cs
+++ Assets/MistNet/Runtime/Scripts/ConnectionSelector/GossipConnectionSelector.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+
 namespace MistNet
 {
     public class GossipConnectionSelector : IConnectionSelector
@@ -15,6 +20,35 @@ namespace MistNet
         protected override void OnMessage(string data, string id)
         {
             MistDebug.Log($"[GossipConnectionSelector] OnMessage {id}");
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+
+        }
+
+        private async UniTask UpdateSendMessage(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                await UniTask.Delay(1000, cancellationToken: token);
+                
+            }
+        }
+
+        private void CreateMessageData()
+        {
+
+
+        }
+
+        [Serializable]
+        internal class MessageData
+        {
+            public string type;
+            public Dictionary<string, object> worlds;
+            
         }
     }
 }
