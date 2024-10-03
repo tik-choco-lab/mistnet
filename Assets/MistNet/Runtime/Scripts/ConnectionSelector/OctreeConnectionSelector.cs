b14f11b7026063a2067d60bd5d1fbc736fe096ae
fog <38463346+fog-zs@users.noreply.github.com>
2024-10-03 17:18:06 +0900

    add octree

---
+++ Assets/MistNet/Runtime/Scripts/ConnectionSelector/OctreeConnectionSelector.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MistNet
+{
+    public class OctreeConnectionSelector : IConnectionSelector
+    {
+        private readonly HashSet<string> _connectedNodes = new();
+
+        protected override void Start()
+        {
+            base.Start();
+            Debug.Log($"[ConnectionSelector] SelfId {MistPeerData.I.SelfId}");
+        }
+
+        public override void OnConnected(string id)
+        {
+            Debug.Log($"[ConnectionSelector] OnConnected: {id}");
+            if (!_connectedNodes.Add(id)) return;
+            var dataStr = string.Join(",", _connectedNodes);
+            SendAll(dataStr);
+        }
+
+        public override void OnDisconnected(string id)
+        {
+            Debug.Log($"[ConnectionSelector] OnDisconnected: {id}");
+            _connectedNodes.Remove(id);
+        }
+
+        protected override void OnMessage(string data, string id)
+        {
+            var nodes = data.Split(',');
+            Debug.Log($"[ConnectionSelector] ({nodes.Length}) Nodes: {data}");
+
+            foreach (var nodeId in nodes)
+            {
+                if (nodeId == MistPeerData.I.SelfId) continue;
+                if (!_connectedNodes.Add(nodeId)) continue;
+
+                Debug.Log($"[ConnectionSelector] Connect: {nodeId}");
+
+                // idの大きさを比較
+                if (MistManager.I.CompareId(nodeId))
+                {
+                    MistManager.I.Connect(nodeId).Forget();
+                }
+            }
+        }
+    }
+}
