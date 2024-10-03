dd9cf18505f8c1fc4b44eb1adaae265f030a919b
fog <38463346+fog-zs@users.noreply.github.com>
2024-10-03 17:17:59 +0900

    remove unused code

--- Assets/MistNet/Runtime/Scripts/ConnectionSelector/BasicConnectionSelector.cs
+++ Assets/MistNet/Runtime/Scripts/ConnectionSelector/BasicConnectionSelector.cs
@@ -1,8 +1,4 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 namespace MistNet
@@ -14,13 +10,12 @@ namespace MistNet
         protected override void Start()
         {
             base.Start();
-            Debug.Log($"[BasicConnectionSelector] SelfId {MistPeerData.I.SelfId}");
+            Debug.Log($"[ConnectionSelector] SelfId {MistPeerData.I.SelfId}");
         }
 
         public override void OnConnected(string id)
         {
-            Debug.Log($"[BasicConnectionSelector] OnConnected: {id}");
-            // _connectedNodes.Add(id);
+            Debug.Log($"[ConnectionSelector] OnConnected: {id}");
             if (!_connectedNodes.Add(id)) return;
             var dataStr = string.Join(",", _connectedNodes);
             SendAll(dataStr);
@@ -28,21 +23,21 @@ namespace MistNet
 
         public override void OnDisconnected(string id)
         {
-            Debug.Log($"[BasicConnectionSelector] OnDisconnected: {id}");
+            Debug.Log($"[ConnectionSelector] OnDisconnected: {id}");
             _connectedNodes.Remove(id);
         }
 
         protected override void OnMessage(string data, string id)
         {
             var nodes = data.Split(',');
-            Debug.Log($"[BasicConnectionSelector] ({nodes.Length}) Nodes: {data}");
+            Debug.Log($"[ConnectionSelector] ({nodes.Length}) Nodes: {data}");
 
             foreach (var nodeId in nodes)
             {
                 if (nodeId == MistPeerData.I.SelfId) continue;
                 if (!_connectedNodes.Add(nodeId)) continue;
-                // _connectedNodes.Add(id);
-                Debug.Log($"[BasicConnectionSelector] Connect: {nodeId}");
+
+                Debug.Log($"[ConnectionSelector] Connect: {nodeId}");
 
                 // idの大きさを比較
                 if (MistManager.I.CompareId(nodeId))
