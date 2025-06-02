175df4c709c348ae19959f981102c25d9a64b9c3
fog <38463346+fog-zs@users.noreply.github.com>
2025-06-02 16:05:37 +0900

    Update BuildScript.cs

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -1,127 +1,296 @@
-using UnityEditor;
-using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using UnityEditor;
+using UnityEditor.AddressableAssets.Settings;
+using UnityEditor.Build.Reporting;
+#if UNITY_6000_0_OR_NEWER
+using UnityEditor.Build.Profile;
+#endif
 
-public class BuildScript
+namespace UnityBuilderAction
 {
-    public static void Build()
+    public static class BuildScript
     {
-        var buildTargetArg = GetCommandLineArg("-buildTarget");
-        if (string.IsNullOrEmpty(buildTargetArg))
+        private static readonly string Eol = Environment.NewLine;
+
+        private static readonly string[] Secrets =
+            { "androidKeystorePass", "androidKeyaliasName", "androidKeyaliasPass" };
+
+        public static void Build()
         {
-            Debug.LogError("[Error] No build target specified. Use -buildTarget to specify a target platform.");
-            return;
-        }
+            // Gather values from args
+            Dictionary<string, string> options = GetValidatedOptions();
 
-        if (!System.Enum.TryParse(buildTargetArg, out BuildTarget buildTarget))
-        {
-            Debug.LogError($"[Error] Invalid build target: {buildTargetArg}");
-            return;
-        }
-
-        string[] scenes = null;
-        var sceneOptions = GetCommandLineArg("-scenes");
-        scenes = !string.IsNullOrEmpty(sceneOptions)
-            ? sceneOptions.Split(',')
-            : GetEnabledScenes();
-
-
-        var developmentBuild = GetCommandLineArg("-buildOptions") == "Development";
-        BuildWithOptions(scenes, buildTarget, developmentBuild);
-    }
-
-    private static void BuildWithOptions(string[] scenes, BuildTarget buildTarget, bool developmentBuild)
-    {
-        var buildPath = GetBuildPath(buildTarget, developmentBuild);
-        var buildPlayerOptions = new BuildPlayerOptions
-        {
-            scenes = scenes,
-            locationPathName = buildPath,
-            target = buildTarget,
-            options = developmentBuild
-                ? BuildOptions.Development
-                : BuildOptions.None
-        };
-
-        var buildReport = BuildPipeline.BuildPlayer(buildPlayerOptions);
-        if (buildReport.summary.result == UnityEditor.Build.Reporting.BuildResult.Succeeded)
-        {
-            Debug.Log($"[Success] Build {buildTarget}");
-        }
-        else
-        {
-            Debug.LogError($"[Error] Build {buildTarget} failed. Reason: {buildReport.summary.result}");
-        }
-    }
-
-    /// <summary>
-    /// 有効なシーンを取得するメソッド
-    /// </summary>
-    /// <returns></returns>
-    private static string[] GetEnabledScenes()
-    {
-        var customScenes = GetCommandLineArg("-scenes");
-        if (!string.IsNullOrEmpty(customScenes))
-        {
-            return customScenes.Split(',').Select(scene => scene.Trim()).ToArray();
-        }
-
-        return EditorBuildSettings.scenes
-            .Where(scene => scene.enabled)
-            .Select(scene => scene.path)
-            .ToArray();
-    }
-
-    /// <summary>
-    /// ビルドターゲットに応じた出力先パスを生成するメソッド
-    /// </summary>
-    /// <param name="buildTarget"></param>
-    /// <param name="developmentBuild"></param>
-    /// <returns></returns>
-    private static string GetBuildPath(BuildTarget buildTarget, bool developmentBuild)
-    {
-        var buildDirectory = GetCommandLineArg("-buildDirectory") ?? "Builds/";
-        // debug or release
-        var buildOptions = developmentBuild ? "debug" : "release";
-        buildDirectory = $"{buildDirectory}{buildOptions}/{buildTarget.ToString()}/";
-        var productName = PlayerSettings.productName;
-
-        switch (buildTarget)
-        {
-            case BuildTarget.StandaloneOSX:
-                return $"{buildDirectory}{productName}.app";
-            case BuildTarget.StandaloneWindows:
-                return $"{buildDirectory}{productName}_32.exe";
-            case BuildTarget.StandaloneWindows64:
-                return $"{buildDirectory}{productName}.exe";
-            case BuildTarget.StandaloneLinux64:
-                return $"{buildDirectory}{productName}";
-            case BuildTarget.iOS:
-                return $"{buildDirectory}{productName}";
-            case BuildTarget.Android:
-                return $"{buildDirectory}{productName}.apk";
-            default:
-                Debug.LogError("Unsupported build target.");
-                return $"{buildDirectory}Unknown/{productName}";
-        }
-    }
-
-    /// <summary>
-    /// Command Line引数を取得するメソッド
-    /// </summary>
-    /// <param name="name"></param>
-    /// <returns></returns>
-    private static string GetCommandLineArg(string name)
-    {
-        var args = System.Environment.GetCommandLineArgs();
-        for (var i = 0; i < args.Length; i++)
-        {
-            if (args[i] == name && i + 1 < args.Length)
+            // Set version for this build
+            if (options.TryGetValue("buildVersion", out string buildVersion) && buildVersion != "none")
             {
-                return args[i + 1];
+                PlayerSettings.bundleVersion = buildVersion;
+                PlayerSettings.macOS.buildNumber = buildVersion;
+            }
+
+            if (options.TryGetValue("androidVersionCode", out string versionCode) && versionCode != "0")
+            {
+                PlayerSettings.Android.bundleVersionCode = int.Parse(options["androidVersionCode"]);
+            }
+
+            var isDevelopmentBuild = false;
+            if (options.TryGetValue("developmentBuild", out string developmentBuild))
+            {
+                isDevelopmentBuild = string.IsNullOrEmpty(developmentBuild) || bool.TryParse(developmentBuild, out isDevelopmentBuild) && isDevelopmentBuild;
+            }
+
+            var isAddressablesBuild = false;
+            if (options.TryGetValue("addressablesBuild", out string addressablesBuild))
+            {
+                isAddressablesBuild = string.IsNullOrEmpty(addressablesBuild) || bool.TryParse(addressablesBuild, out isAddressablesBuild) && isAddressablesBuild;
+            }
+
+            // Apply build target
+            var buildTarget = (BuildTarget)Enum.Parse(typeof(BuildTarget), options["buildTarget"]);
+            switch (buildTarget)
+            {
+                case BuildTarget.Android:
+                {
+                    EditorUserBuildSettings.buildAppBundle = options["customBuildPath"].EndsWith(".aab");
+                    if (options.TryGetValue("androidKeystoreName", out string keystoreName) &&
+                        !string.IsNullOrEmpty(keystoreName))
+                    {
+                        PlayerSettings.Android.useCustomKeystore = true;
+                        PlayerSettings.Android.keystoreName = keystoreName;
+                    }
+
+                    if (options.TryGetValue("androidKeystorePass", out string keystorePass) &&
+                        !string.IsNullOrEmpty(keystorePass))
+                        PlayerSettings.Android.keystorePass = keystorePass;
+                    if (options.TryGetValue("androidKeyaliasName", out string keyaliasName) &&
+                        !string.IsNullOrEmpty(keyaliasName))
+                        PlayerSettings.Android.keyaliasName = keyaliasName;
+                    if (options.TryGetValue("androidKeyaliasPass", out string keyaliasPass) &&
+                        !string.IsNullOrEmpty(keyaliasPass))
+                        PlayerSettings.Android.keyaliasPass = keyaliasPass;
+                    if (options.TryGetValue("androidTargetSdkVersion", out string androidTargetSdkVersion) &&
+                        !string.IsNullOrEmpty(androidTargetSdkVersion))
+                    {
+                        var targetSdkVersion = AndroidSdkVersions.AndroidApiLevelAuto;
+                        try
+                        {
+                            targetSdkVersion =
+                                (AndroidSdkVersions)Enum.Parse(typeof(AndroidSdkVersions), androidTargetSdkVersion);
+                        }
+                        catch
+                        {
+                            UnityEngine.Debug.Log(
+                                "Failed to parse androidTargetSdkVersion! Fallback to AndroidApiLevelAuto");
+                        }
+
+                        PlayerSettings.Android.targetSdkVersion = targetSdkVersion;
+                    }
+
+
+                    break;
+                }
+                case BuildTarget.StandaloneOSX:
+                    PlayerSettings.SetScriptingBackend(BuildTargetGroup.Standalone, ScriptingImplementation.Mono2x);
+                    break;
+            }
+
+            // Determine subtarget
+            int buildSubtarget = 0;
+#if UNITY_2021_2_OR_NEWER
+            if (!options.TryGetValue("standaloneBuildSubtarget", out var subtargetValue) ||
+                !Enum.TryParse(subtargetValue, out StandaloneBuildSubtarget buildSubtargetValue))
+            {
+                buildSubtargetValue = default;
+            }
+
+            buildSubtarget = (int)buildSubtargetValue;
+#endif
+
+            // Custom build
+            Build(buildTarget, buildSubtarget, options["customBuildPath"], isDevelopmentBuild, isAddressablesBuild);
+        }
+
+#if UNITY_6000_0_OR_NEWER
+        public static void BuildWithProfile()
+        {
+            // Gather values from args
+            Dictionary<string, string> options = GetValidatedOptions();
+
+            // Load build profile from Assets folder
+            BuildProfile buildProfile = AssetDatabase.LoadAssetAtPath<BuildProfile>(options["customBuildProfile"]);
+
+            // Set it as active
+            BuildProfile.SetActiveBuildProfile(buildProfile);
+// Get all buildOptions from options
+            BuildOptions buildOptions = BuildOptions.None;
+            foreach (string buildOptionString in Enum.GetNames(typeof(BuildOptions)))
+            {
+                if (options.ContainsKey(buildOptionString))
+                {
+                    BuildOptions buildOptionEnum = (BuildOptions)Enum.Parse(typeof(BuildOptions), buildOptionString);
+                    buildOptions |= buildOptionEnum;
+                }
+            }
+
+            // Define BuildPlayerWithProfileOptions
+            var buildPlayerWithProfileOptions = new BuildPlayerWithProfileOptions
+            {
+                buildProfile = buildProfile,
+                locationPathName = options["customBuildPath"],
+                options = buildOptions,
+            };
+
+            BuildSummary buildSummary = BuildPipeline.BuildPlayer(buildPlayerWithProfileOptions).summary;
+            ReportSummary(buildSummary);
+            ExitWithResult(buildSummary.result);
+        }
+#endif
+
+        private static Dictionary<string, string> GetValidatedOptions()
+        {
+            ParseCommandLineArguments(out Dictionary<string, string> validatedOptions);
+
+            if (!validatedOptions.TryGetValue("projectPath", out string _))
+            {
+                Console.WriteLine("Missing argument -projectPath");
+                EditorApplication.Exit(110);
+            }
+
+            if (!validatedOptions.TryGetValue("buildTarget", out string buildTarget))
+            {
+                Console.WriteLine("Missing argument -buildTarget");
+                EditorApplication.Exit(120);
+            }
+
+            if (!Enum.IsDefined(typeof(BuildTarget), buildTarget ?? string.Empty))
+            {
+                Console.WriteLine($"{buildTarget} is not a defined {nameof(BuildTarget)}");
+                EditorApplication.Exit(121);
+            }
+
+            if (!validatedOptions.TryGetValue("customBuildPath", out string _))
+            {
+                Console.WriteLine("Missing argument -customBuildPath");
+                EditorApplication.Exit(130);
+            }
+
+            const string defaultCustomBuildName = "TestBuild";
+            if (!validatedOptions.TryGetValue("customBuildName", out string customBuildName))
+            {
+                Console.WriteLine($"Missing argument -customBuildName, defaulting to {defaultCustomBuildName}.");
+                validatedOptions.Add("customBuildName", defaultCustomBuildName);
+            }
+            else if (customBuildName == "")
+            {
+                Console.WriteLine($"Invalid argument -customBuildName, defaulting to {defaultCustomBuildName}.");
+                validatedOptions.Add("customBuildName", defaultCustomBuildName);
+            }
+
+            return validatedOptions;
+        }
+
+        private static void ParseCommandLineArguments(out Dictionary<string, string> providedArguments)
+        {
+            providedArguments = new Dictionary<string, string>();
+            string[] args = Environment.GetCommandLineArgs();
+
+            Console.WriteLine(
+                $"{Eol}" +
+                $"###########################{Eol}" +
+                $"#    Parsing settings     #{Eol}" +
+                $"###########################{Eol}" +
+                $"{Eol}"
+            );
+
+            // Extract flags with optional values
+            for (int current = 0, next = 1; current < args.Length; current++, next++)
+            {
+                // Parse flag
+                bool isFlag = args[current].StartsWith("-");
+                if (!isFlag) continue;
+                string flag = args[current].TrimStart('-');
+
+                // Parse optional value
+                bool flagHasValue = next < args.Length && !args[next].StartsWith("-");
+                string value = flagHasValue ? args[next].TrimStart('-') : "";
+                bool secret = Secrets.Contains(flag);
+                string displayValue = secret ? "*HIDDEN*" : "\"" + value + "\"";
+
+                // Assign
+                Console.WriteLine($"Found flag \"{flag}\" with value {displayValue}.");
+                providedArguments.Add(flag, value);
             }
         }
 
-        return null;
+        private static void Build(BuildTarget buildTarget, int buildSubtarget, string filePath, bool isDevelopmentBuild, bool isAddressablesBuild)
+        {
+            if (isAddressablesBuild)
+            {
+                // Build Addressables
+                Console.WriteLine("Building Addressables...");
+                AddressableAssetSettings.CleanPlayerContent();
+                AddressableAssetSettings.BuildPlayerContent();
+                Console.WriteLine("Addressables build completed.");
+            }
+
+            string[] scenes = EditorBuildSettings.scenes.Where(scene => scene.enabled).Select(s => s.path).ToArray();
+            var buildPlayerOptions = new BuildPlayerOptions
+            {
+                scenes = scenes,
+                target = buildTarget,
+//                targetGroup = BuildPipeline.GetBuildTargetGroup(buildTarget),
+                locationPathName = filePath,
+                options = isDevelopmentBuild ? UnityEditor.BuildOptions.Development : UnityEditor.BuildOptions.None,
+#if UNITY_2021_2_OR_NEWER
+                subtarget = buildSubtarget
+#endif
+            };
+
+            BuildSummary buildSummary = BuildPipeline.BuildPlayer(buildPlayerOptions).summary;
+            ReportSummary(buildSummary);
+            ExitWithResult(buildSummary.result);
+        }
+
+        private static void ReportSummary(BuildSummary summary)
+        {
+            Console.WriteLine(
+                $"{Eol}" +
+                $"###########################{Eol}" +
+                $"#      Build results      #{Eol}" +
+                $"###########################{Eol}" +
+                $"{Eol}" +
+                $"Duration: {summary.totalTime.ToString()}{Eol}" +
+                $"Warnings: {summary.totalWarnings.ToString()}{Eol}" +
+                $"Errors: {summary.totalErrors.ToString()}{Eol}" +
+                $"Size: {summary.totalSize.ToString()} bytes{Eol}" +
+                $"{Eol}"
+            );
+        }
+
+        private static void ExitWithResult(BuildResult result)
+        {
+            switch (result)
+            {
+                case BuildResult.Succeeded:
+                    Console.WriteLine("Build succeeded!");
+                    EditorApplication.Exit(0);
+                    break;
+                case BuildResult.Failed:
+                    Console.WriteLine("Build failed!");
+                    EditorApplication.Exit(101);
+                    break;
+                case BuildResult.Cancelled:
+                    Console.WriteLine("Build cancelled!");
+                    EditorApplication.Exit(102);
+                    break;
+                case BuildResult.Unknown:
+                default:
+                    Console.WriteLine("Build result is unknown!");
+                    EditorApplication.Exit(103);
+                    break;
+            }
+        }
     }
 }
