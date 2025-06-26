01c79cc61ec262db9bd59283287840f80cb4942b
fog <38463346+fog-zs@users.noreply.github.com>
2025-06-26 16:44:47 +0900

    remove unused code

--- Assets/MistNet/Runtime/Scripts/Utils/NodeUtils.cs
+++ Assets/MistNet/Runtime/Scripts/Utils/NodeUtils.cs
@@ -64,40 +64,5 @@ namespace MistNet.Utils
             }
             return nodeArray;
         }
-
-        // public static Node[] GetOtherNodeData()
-        // {
-        //     var connectedNodes = MistManager.I.routing.ConnectedNodes;
-        //     var objects = MistSyncManager.I.ObjectIdsByOwnerId;
-        //
-        //     var i = 0;
-        //     var nodes = new Node[connectedNodes.Count];
-        //     foreach (var nodeId in connectedNodes)
-        //     {
-        //         if (nodeId == PeerRepository.I.SelfId) continue;
-        //         var node = MistManager.I.routing.GetNode(nodeId);
-        //         var position = node?.Position ?? new Position(0, 0, 0);
-        //         var state = EvalNodeState.Connected;
-        //         if (objects.TryGetValue(nodeId, out var obj))
-        //         {
-        //             var firstObjectId = obj[0];
-        //             var firstObject = MistSyncManager.I.GetSyncObject(firstObjectId);
-        //             if (firstObject != null)
-        //             {
-        //                 position = new Position(firstObject.transform.position);
-        //                 state = EvalNodeState.Visible;
-        //             }
-        //         }
-        //         nodes[i] = new Node(
-        //             nodeId: nodeId,
-        //             position: position,
-        //             state: state
-        //         );
-        //
-        //         i++;
-        //     }
-        //
-        //     return nodes;
-        // }
     }
 }
