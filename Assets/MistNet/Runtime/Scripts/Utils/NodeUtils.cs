47ae4626867f87b188359c7328a2405e54438c9a
fog <38463346+fog-zs@users.noreply.github.com>
2025-06-26 16:45:35 +0900

    remove unused code

--- Assets/MistNet/Runtime/Scripts/Utils/NodeUtils.cs
+++ Assets/MistNet/Runtime/Scripts/Utils/NodeUtils.cs
@@ -12,26 +12,6 @@ namespace MistNet.Utils
             return node;
         }
 
-        public static Node[] GetAllNodeData()
-        {
-            var objects = MistSyncManager.I.ObjectIdsByOwnerId;
-            var nodes = new Node[objects.Count];
-
-            var i = 0;
-            foreach (var (nodeId, objectList) in objects)
-            {
-                var firstObjectId = objectList[0];
-                var firstObject = MistSyncManager.I.GetSyncObject(firstObjectId);
-                nodes[i] = new Node(
-                    nodeId: nodeId,
-                    position: new Position(firstObject.transform.position)
-                );
-                i++;
-            }
-
-            return nodes;
-        }
-
         public static Node[] GetOtherNodeData()
         {
             var nodes = MistManager.I.routing.Nodes;
