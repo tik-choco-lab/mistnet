8b51fbfb0d4904cb017e1fec9604affab8e2f362
fog <38463346+fog-zs@users.noreply.github.com>
2025-07-07 15:22:20 +0900

    remove unused using

--- Assets/MistNet/Runtime/Scripts/Test/PlayerTest.cs
+++ Assets/MistNet/Runtime/Scripts/Test/PlayerTest.cs
@@ -1,22 +1,21 @@
 using System;
-using UnityEngine;
 
 namespace MistNet.Test
 {
     public class PlayerTest : MistBehaviour
     {
         [MistSync(OnChanged = nameof(OnChanged))]
-        private string playerName { get; set; }
+        private string PlayerName { get; set; }
 
         private void OnChanged()
         {
-            MistDebug.Log($"[Test] Player name changed to: {playerName}");
+            MistDebug.Log($"[Test] Player name changed to: {PlayerName}");
         }
 
         protected override void LocalStart()
         {
-            playerName = "Player_" + Guid.NewGuid().ToString("N").Substring(0, 4);
-            MistDebug.Log($"[Test] Player initialized with name: {playerName}");
+            PlayerName = "Player_" + Guid.NewGuid().ToString("N").Substring(0, 4);
+            MistDebug.Log($"[Test] Player initialized with name: {PlayerName}");
         }
     }
 }
