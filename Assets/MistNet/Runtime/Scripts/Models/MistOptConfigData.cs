a84af492e72e77e72e515ca174cea1aa53c7f0de
fog <38463346+fog-zs@users.noreply.github.com>
2025-09-18 21:40:25 +0900

    不要な変数を削除

--- Assets/MistNet/Runtime/Scripts/Models/MistOptConfigData.cs
+++ Assets/MistNet/Runtime/Scripts/Models/MistOptConfigData.cs
@@ -6,23 +6,15 @@ namespace MistNet
     {
         [JsonProperty("visibleCount")] public int VisibleCount = 5;
         [JsonProperty("maxConnectionCount")] public int MaxConnectionCount = 20;
+
+        // DNVE1
         [JsonProperty("safeMargin")] public int SafeMargin = 3;
-
-        [JsonProperty("bucketBase")] public int BucketBase = 4;
-        [JsonProperty("bucketMax")] public int BucketMax = 5;
-        [JsonProperty("bucketIndexMax")] public int BucketIndexMax = 4;
-
         [JsonProperty("chunkLoadSize")] public int ChunkLoadSize = 2;
-        [JsonProperty("connectionsPerBucket")] public int ConnectionsPerBucket = 2;
-        [JsonProperty("sendInfoIntervalSecondMultiplier")] public int SendInfoIntervalSecondMultiplier = 3;
-
-        [JsonProperty("pingTimeoutSeconds")] public int PingTimeoutSeconds = 5;
-        [JsonProperty("requestObjectIntervalSeconds")] public int RequestObjectIntervalSeconds = 5;
         [JsonProperty("areaTrackerIntervalSeconds")] public float AreaTrackerIntervalSeconds = 5f;
         [JsonProperty("connectionBalancerIntervalSeconds")] public float ConnectionBalancerIntervalSeconds = 2f;
         [JsonProperty("visibleNodesIntervalSeconds")] public float VisibleNodesIntervalSeconds = 2f;
-        [JsonProperty("nodeListExchangeNeighborMaxCount")] public int NodeListExchangeNeighborMaxCount = 3;
 
+        // DNVE2
         [JsonProperty("nodeListExchangeIntervalSeconds")] public float NodeListExchangeIntervalSeconds = 3f;
         [JsonProperty("nodeListExchangeMaxCount")] public int NodeListExchangeMaxCount = 10;
     }
