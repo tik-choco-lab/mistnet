3c3563072ebf82c3ec757650103926a4d626486e
fog <38463346+fog-zs@users.noreply.github.com>
2026-01-13 13:38:53 +0900

    40 -> 20

--- Assets/MistNet/Runtime/Scripts/Models/MistOptConfigData.cs
+++ Assets/MistNet/Runtime/Scripts/Models/MistOptConfigData.cs
@@ -5,7 +5,7 @@ namespace MistNet
     public class MistOptConfigData
     {
         [JsonProperty("visibleCount")] public int VisibleCount = 5;
-        [JsonProperty("maxConnectionCount")] public int MaxConnectionCount = 40;
+        [JsonProperty("maxConnectionCount")] public int MaxConnectionCount = 20;
 
         // DNVE1 ---------------
         [JsonProperty("chunkLoadSize")] public int ChunkLoadSize = 1;
